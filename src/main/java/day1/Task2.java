1e324402088e6d3db28035a454f5f04d35365829
properk <properk34@yandex.ru>
2020-12-24 20:03:57 +0300

    day1.Task2

--- src/main/java/day1/Task2.java
+++ src/main/java/day1/Task2.java
@@ -1,7 +1,14 @@
 package day1;
 
 public class Task2 {
-    public static void main(String[] args) {
 
+    public static void tenJava(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.print("JAVA ");
+        }
+    }
+
+    public static void main(String[] args) {
+        tenJava(10);
     }
 }
