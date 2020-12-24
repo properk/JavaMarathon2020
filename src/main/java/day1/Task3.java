ad59f1367747e7982c139e1a40892852c1f5c33a
properk <properk34@yandex.ru>
2020-12-24 20:10:13 +0300

    Add day1.Task3 solutions

--- src/main/java/day1/Task3.java
+++ src/main/java/day1/Task3.java
@@ -1,7 +1,14 @@
 package day1;
 
 public class Task3 {
-    public static void main(String[] args) {
 
+    public static void tenJava(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println("JAVA");
+        }
+    }
+
+    public static void main(String[] args) {
+        tenJava(10);
     }
 }
