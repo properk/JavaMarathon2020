6bf19320a0025db21fbfdd61b3db55dea6ce55d9
properk <properk34@yandex.ru>
2020-12-25 19:24:37 +0300

    Add day2.Task1 solution Add day2.Task2 solution Add day2.Task3 solution Add day2.Task4 solution

--- src/main/java/day2/Task4.java
+++ src/main/java/day2/Task4.java
@@ -1,7 +1,19 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task4 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double x = scanner.nextDouble();
+        scanner.close();
 
+        double y;
+
+        if (x >= 5) {
+            System.out.println(y = (x * x - 10) / (x + 7));
+        } else if (x > -3 && x < 5) {
+            System.out.println(y = (x + 3) * (x * x - 2));
+        } else System.out.println(y = 420);
     }
 }
